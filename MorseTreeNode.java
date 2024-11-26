b9cfaceadbfa977408552fd6bd4554fa3fe9428a
MrGlayga <ibnOualid@proton.me>
2024-11-26 11:19:14 +0100

    ascii-morse y morse-ascii

--- MorseTreeNode.java
+++ MorseTreeNode.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 class MorseTreeNode {
@@ -25,7 +26,46 @@ class MorseTreeNode {
                 System.out.println("Morse Code: " + code + " -> Letter: " + letter);
             }
 
-            scanner.close();
+            HashMap<Character, String> asciiMorse = new HashMap<>();
+            asciiMorse.put('a', "._");
+            asciiMorse.put('b', "_...");
+            asciiMorse.put('c', "_._.");
+            asciiMorse.put('d', "_..");
+            asciiMorse.put('e', ".");
+            asciiMorse.put('f', ".._.");
+            asciiMorse.put('g', "__.");
+            asciiMorse.put('h', "....");
+            asciiMorse.put('i', "..");
+            asciiMorse.put('j', ".___");
+            asciiMorse.put('k', "_._");
+            asciiMorse.put('l', "._..");
+            asciiMorse.put('m', "__");
+            asciiMorse.put('n', "_.");
+            asciiMorse.put('ñ', "__.__");
+            asciiMorse.put('o', "___");
+            asciiMorse.put('p', ".__.");
+            asciiMorse.put('q', "__._");
+            asciiMorse.put('r', "._.");
+            asciiMorse.put('s', "...");
+            asciiMorse.put('t', "_");
+            asciiMorse.put('u', ".._");
+            asciiMorse.put('v', "..._");
+            asciiMorse.put('w', ".__");
+            asciiMorse.put('x', "_.._");
+            asciiMorse.put('y', "");
+            asciiMorse.put('z', "__..");
+
+            System.out.println("Escribe una frase, la traducire a morse");
+
+            String frase = scanner.nextLine();
+
+            for (int i = 0; i < frase.length(); i++){
+                if (frase.charAt(i) != ' ' ){
+                    System.out.print(asciiMorse.get(frase.charAt(i)) + " ");
+                }else{
+                    System.out.print(" ");
+                }
+            }
         }
 
 }
